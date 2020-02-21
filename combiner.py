272808b85e4fb7955ade129839d6dab16d4684c2
Zhongjie Shen <zhongjie.shen@bchydro.com>
2020-02-21 15:53:41 -0800

    Wrap most functions in classes.

--- combiner.py
+++ combiner.py
@@ -1,13 +1,21 @@
 from os import listdir
 from os.path import isfile, join
-
 import pandas as pd
 
-csv_dir = './download/2020-02-21-105226/'
-files = [f for f in listdir(csv_dir) if isfile(join(csv_dir, f))]
-print(files)
 
-# combine all files in the list
-combined_csv = pd.concat([pd.read_csv(csv_dir + f) for f in files])
-# export to csv
-combined_csv.to_csv("combined_csv.csv", index=False, encoding='utf-8')
+class CSVCombiner:
+    csv_dir = None
+    output_file = None
+
+    def __init__(self, source_dir, output_file):
+        self.csv_dir = source_dir
+        self.output_file = output_file
+
+    def run(self):
+        files = [f for f in listdir(self.csv_dir) if isfile(join(self.csv_dir, f))]
+        print(files)
+
+        # combine all files in the list
+        combined_csv = pd.concat([pd.read_csv(self.csv_dir + f) for f in files])
+        # export to csv
+        combined_csv.to_csv(self.output_file, index=False, encoding='utf-8')
--- downloader.py
+++ downloader.py
@@ -1,4 +1,3 @@
-import csv
 import datetime
 import logging
 import os
@@ -10,70 +9,107 @@ from urllib.parse import urlencode
 
 import requests
 
-st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H%M%S')
+from proxies import Proxies
 
 logging.basicConfig(level=logging.INFO)
 
-# User-defined proxies
-proxies = {
-    "http": "",
-    "https": "",
-}
 
+class CanWeatherDataDownloader:
+    st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d-%H%M%S')
+    download_dir = "./download/" + st + "/"
+    output_format = 'csv'
+    station_id_list = []
 
-# Find URL for a single station id in a year
-def build_data_download_url(output_format, station_id, year, month=1, day=1, time_frame=2):
-    endpoint_url = "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?"
-    params = {'format': output_format, 'stationID': station_id, 'Year': year, 'Month': month,
-              'Day': day,
-              'timeframe': time_frame}
+    def __init__(self):
+        if not os.path.exists(self.download_dir):
+            os.makedirs(self.download_dir)
 
-    url_parts = list(urlparse.urlparse(endpoint_url))
-    query = dict(urlparse.parse_qsl(url_parts[4]))
-    query.update(params)
+    @staticmethod
+    def build_data_download_url(output_format, station_id, year, month, day, time_frame):
+        """
+        Build the downloadable URL.
 
-    url_parts[4] = urlencode(query)
+        @param output_format: Output format
+        @param station_id: Station ID
+        @param year: Year
+        @param month: Month
+        @param day: Day
+        @param time_frame: Time Frame. 1 for hourly, 2 for daily, 3 for monthly.
+        @return: A downloadable url
+        """
+        endpoint_url = "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?"
+        params = {'format': output_format, 'stationID': station_id, 'Year': year, 'Month': month,
+                  'Day': day,
+                  'timeframe': time_frame}
 
-    url_to_get = urlparse.urlunparse(url_parts)
-    # print(url_to_get)
-    return url_to_get
+        url_parts = list(urlparse.urlparse(endpoint_url))
+        query = dict(urlparse.parse_qsl(url_parts[4]))
+        query.update(params)
 
+        url_parts[4] = urlencode(query)
 
-# Read all station IDs from CSV
-with open('station_id.csv', newline='') as id_f:
-    reader = csv.reader(id_f)
-    bc_station_id_list = tuple(reader)
-    logging.info("Total number of stations: " + str(len(bc_station_id_list)))
+        url_to_get = urlparse.urlunparse(url_parts)
+        # print(url_to_get)
+        return url_to_get
 
-download_urls = []
-for y in range(1990, 2021):
-    for s_id in bc_station_id_list:
-        s_url = build_data_download_url('csv', s_id[0], y)
-        download_urls.append(s_url)
-logging.info("Total number of URLs: " + str(len(download_urls)))
+    @staticmethod
+    def read_station_id_from_file(file):
+        """
+        Read all station IDs from file
 
-download_dir = "./download/" + st + "/"
-if not os.path.exists(download_dir):
-    os.makedirs(download_dir)
+        @param file: File contains station IDs
+        @return: A list contains all the station IDs
+        """
+        with open(file) as id_f:
+            station_id_list = id_f.readlines()
+            logging.info("Total number of stations: " + str(len(station_id_list)))
+        return station_id_list
 
+    @staticmethod
+    def download_from_url(url, destination):
+        """
+        Download single file from URL using the file name from 'content-disposition'.
+        @type url: str
+        @type destination: str
 
-# Download file from URL using the file name from content-disposition
-def download_url(url):
-    print("downloading: ", url)
+        @param url: URL to download
+        @param destination: Destination folder to store file
+        """
+        logging.info("downloading: ", url)
 
-    r = requests.get(url, proxies=proxies, stream=True)
-    d = r.headers['content-disposition']
-    fname: str = re.findall("filename=(.+)", d)[0]
+        r = requests.get(url, proxies=Proxies.get_random_proxy(), stream=True)
+        d = r.headers['content-disposition']
+        fname: str = re.findall("filename=(.+)", d)[0]
 
-    if r.status_code == requests.codes.ok:
-        with open(download_dir + fname.replace('"', ''), 'wb+') as download_f:
-            for data in r:
-                download_f.write(data)
-    return url
+        if r.status_code == requests.codes.ok:
+            with open(destination + fname.replace('"', ''), 'wb+') as download_f:
+                for data in r:
+                    download_f.write(data)
+        return url
+
+    def download_daily_data(self, station_id_list, start_year, end_year):
+        """
+        Download daily data between start year and end year (inclusive)
+        @type station_id_list: list
+        @type start_year: int
+        @type end_year: int
+
+        @param station_id_list: A list contains station ID. One for each list element
+        @param start_year: start year (inclusive)
+        @param end_year: end year (inclusive)
+        """
+        download_urls = []
+        for y in range(start_year, end_year + 1):
+            for s_id in station_id_list:
+                s_url = self.build_data_download_url(self.output_format, s_id[0], y, 1, 1, 2)
+                download_urls.append(s_url)
+        logging.info("Total number of URLs: " + str(len(download_urls)))
+        for u in download_urls:
+            self.download_from_url(u, self.download_dir)
 
 
 # Run multiple threads. Each call will take the next element in urls list
-results = ThreadPool(50).imap_unordered(download_url, download_urls)
+results = ThreadPool(1).imap_unordered(download_from_url, download_urls)
 for r in results:
     print(r)
 
