477089e6dda0a242dc57e4e6e72893eea58603f7
Zhongjie Shen <zhongjie.shen@bchydro.com>
2020-03-12 15:11:08 -0700

    Added CSV encoding parameters.

--- combiner.py
+++ combiner.py
@@ -1,21 +1,28 @@
 from os import listdir
 from os.path import isfile, join
+
 import pandas as pd
 
 
 class CSVCombiner:
-    csv_dir = None
-    output_file = None
 
     def __init__(self, source_dir, output_file):
         self.csv_dir = source_dir
         self.output_file = output_file
+        self.input_encoding = 'utf-8'
+        self.output_encoding = 'utf-8'
+
+    def set_input_encoding(self, encoding):
+        self.input_encoding = encoding
+
+    def set_output_encoding(self, encoding):
+        self.output_encoding = encoding
 
     def run(self):
         files = [f for f in listdir(self.csv_dir) if isfile(join(self.csv_dir, f))]
         print(files)
 
         # combine all files in the list
-        combined_csv = pd.concat([pd.read_csv(self.csv_dir + f) for f in files])
+        combined_csv = pd.concat([pd.read_csv(self.csv_dir + f, encoding=self.input_encoding) for f in files])
         # export to csv
-        combined_csv.to_csv(self.output_file, index=False, encoding='utf-8')
+        combined_csv.to_csv(self.output_file, index=False, encoding=self.output_encoding)
